c56cf495d7af48b7ffe7da891ff15310b2a72f9d
Adeel Ansari <adeel.ansari@my.experian.com>
2014-01-23 17:36:23 +0800

    Change the use of deparecated contrib.math library with the new math.numeric-tower. Add comments to few functions. And add new function 'pseudoprime?'.

--- src/main/clojure/arithmetic/arithmetic.clj
+++ src/main/clojure/arithmetic/arithmetic.clj
@@ -3,7 +3,7 @@
      :doc "Empty"}
   clojure.math.arithmetic
   (:require [clojure.math.combinatorics :as comb])
-  (use [clojure.contrib.math]))
+  (use [clojure.math.numeric-tower]))
 
 (defn mult-of-neibr-of-sixers? [x]
   (loop [[d & more] (range 6 (+ (sqrt x) 2) 6)]
@@ -14,6 +14,7 @@
       false)))
 
 (defn subsets
+  "It's better to use math.combinatorics/subsets, instead"
   ([s]
      (if (empty? s)
        '(())
@@ -25,7 +26,9 @@
         (not (nil? s)) (subsets (first s) (next s) nres)
         :else nres))))
 
-(defn prime? [n]
+(defn prime?
+  "A trivial, rigorous, and straight forward approach. Very inefficient for large primes."
+  [n]
   (if (<= n 1)
     false
     (if (or (= n 2) (= n 3))
@@ -34,7 +37,16 @@
         false
         (not (mult-of-neibr-of-sixers? n))))))
 
-(defn next-prime [n]
+(defn pseudoprime?
+  "Fermat Pseudoprime"
+  [n base]
+  (if (or (prime? n) (= n 1))
+    false
+    (= 0 (rem (- (expt base (- n 1)) 1) n))))
+
+(defn next-prime
+  "A trivial, rigorous, and straight forward approach. Very inefficient for large numbers."
+  [n]
   (if (< n 2)
     2
     (loop [k (if (odd? n) (+ n 2) (inc n))]
@@ -42,14 +54,18 @@
         k
         (recur (+ k 2))))))
 
-(defn next-nprimes [from n]
+(defn next-nprimes
+  "A trivial and straight forward approach. Throws exception for n > 100."
+  [from n]
   (if (< (* from n) 0)
     (throw (Exception. "Both 'from' and 'n' must be positive."))
     (if (> n 100)
       (throw (Exception. "Range exceeded the limit of 100."))
       (->> (iterate next-prime from) rest (take n) sort))))
 
-(defn prime-factors [n]
+(defn prime-factors
+  "A trivial and straight forward approach. Very inefficient for large numbers."
+  [n]
   (if (< 1 n)
     (loop [n n
            fcts ()
@@ -60,9 +76,6 @@
          (zero? (rem n d)) (recur q (conj fcts d) d)
          :else (recur n fcts (inc d)))))
     []))
-
-(defn dist-prime-factors [n]
-  (distinct (prime-factors n)))
 
 (defn factors [n]
   (let [ ssets (comb/subsets (prime-factors n))]
@@ -97,12 +110,9 @@
          (>= p (+ sig 1)) false
          :else (recur (first ps) (next ps) (sigma (reduce * (next ps)))))))))
 
-(defn totient [n]
-  1)
-
-(defn prob-u3-1_6 [till d]
-  (let [s (bigint 1) till (inc till)]
-    (loop [t (bigint 2) res 0]
-      (if (<= t till)
-        (recur (inc t) (+ res (reduce * (range s t))))
-        (rem res 7)))))
+(defn totient
+  "Partially implemented, just for primes"
+  [n]
+  (if (prime? n)
+    (dec n)
+    nil))
